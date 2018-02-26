6754b2c0bd5b8aff05299fedc43f050c8c5d0d1e
Pete Johnson <36604219+PeteAJohnson@users.noreply.github.com>
2018-02-25 19:34:26 -0600

    Create BooWho.js

---
+++ BooWho.js
@@ -0,0 +1,12 @@
+// https://www.freecodecamp.org/challenges/boo-who
+
+function booWho(bool) {
+  // What is the new fad diet for ghost developers? The Boolean.
+  
+  if (bool === true || bool === false) {
+    return true;
+  }
+  return false;
+}
+
+booWho(null);
